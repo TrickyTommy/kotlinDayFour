d7d6f1ba4c076c4d462a192677007f8e8ed12e7f
Tommy Ferdian Hadimarta <trickytommy4@gmail.com>
2020-11-05 22:39:21 +0700

    dayFourKotlin
    dayFourKotlin

---
+++ numberTwo.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main (){
+    val lyric = "Aku ingin begini\n" +
+            "Aku ingin begitu\n" +
+            "Ingin ini itu banyak sekali\n\n" +
+            "Semua semua semua\n" +
+            "Dapat dikabulkan\n" +
+            "Dapat dikabulkan\n" +
+            "Dengan kantong ajaib\n\n" +
+            "Aku ingin terbang bebas\n" +
+            "Di angkasa\n" +
+            "Hei… baling baling bambu\n\n" +
+            "La... la... la...\n" +
+            "Aku sayang sekali\n" +
+            "Doraemon\n\n" +
+            "La... la... la...\n" +
+            "Aku sayang sekali";
+}
