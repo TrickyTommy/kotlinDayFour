891904402ebb5e8d5b4a428095276c15c3a96265
Tommy Ferdian Hadimarta <trickytommy4@gmail.com>
2020-11-06 06:48:55 +0700

    nomor dua

--- numberTwo.kt
+++ numberTwo.kt
@@ -1,6 +1,4 @@
-import java.util.*
-
-fun main (){
+fun main() {
     val lyric = "Aku ingin begini\n" +
             "Aku ingin begitu\n" +
             "Ingin ini itu banyak sekali\n\n" +
@@ -15,5 +13,29 @@ fun main (){
             "Aku sayang sekali\n" +
             "Doraemon\n\n" +
             "La... la... la...\n" +
-            "Aku sayang sekali";
+            "Aku sayang sekali"
+
+    var a = countWords(lyric)
+    print(a)
+}
+
+fun countWords(text: String): Map<String, Int>{
+    val words = listOf("aku", "ingin", "la...")
+    val count = mutableMapOf<String, Int>()
+
+    for (i in words){
+        count.put(i, findWord(i, text))
+    }
+    return count
+}
+
+fun findWord(word: String, text: String): Int {
+    var from = 0
+    var count = 0
+    while(text.toLowerCase().indexOf(word, from) > -1){
+        from = text.toLowerCase().indexOf(word, from)
+        ++count
+        from++
+    }
+    return count
 }
